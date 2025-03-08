e866d93712d40c9bc2429a8135baa416b8e981ec
Alexander Tolochko <twist999@gmail.com>
2025-03-08 14:09:58 +0300

    Fix compatibility with M2.4.5

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -4,11 +4,6 @@
  */
 
 var config = {
-    map: {
-        '*': {
-            'Magento_Backend/js/media-uploader': 'MagestyApps_WebImages/js/media-uploader'
-        }
-    },
     config: {
         mixins: {
             'Magento_Ui/js/form/element/image-uploader': {
